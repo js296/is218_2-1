6ea6cf533c7a5b61b4c5b871538490b62274ffaf
js296 <js296@njit.edu>
2017-02-12 17:46:42 -0500

    Part 5: Formatted the numeric variables with currency and percentage formats.

--- display_discount.php
+++ display_discount.php
@@ -1,10 +1,18 @@
 <?php
-
+//get the data from the form
 $product_description = filter_input(INPUT_POST, 'product_description');
 $list_price = filter_input(INPUT_POST, 'list_price');
 $discount_percentage = filter_input(INPUT_POST, 'discount_percent');
+
+// calculate the discount and dicounted price
 $discount_amount = $list_price * ($discount_percentage * .01);
 $discount_price = $list_price - $discount_amount;
+
+//apply currency formatting to the dollar and percent amounts
+$list_price_f = "$".number_format($list_price, 2);
+$discount_percentage_f = $discount_percentage."%";
+$discount_amount_f = "$".number_format($discount_amount, 2);
+$discount_price_f = "$".number_format($discount_price, 2);
 
 ?>
 <!DOCTYPE html>
@@ -21,16 +29,16 @@ $discount_price = $list_price - $discount_amount;
         <span><?php echo $product_description; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo $list_price; ?></span><br>
+        <span><?php echo $list_price_f; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo $discount_percentage; ?></span><br>
+        <span><?php echo $discount_percentage_f; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo $discount_amount; ?></span><br>
+        <span><?php echo $discount_amount_f; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo $discount_price; ?></span><br>
+        <span><?php echo $discount_price_f; ?></span><br>
     </main>
 </body>
 </html>
