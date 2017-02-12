8e14f4c27d5b5ad601c3de927f0b3e8f0cff9bd6
js296 <js296@njit.edu>
2017-02-12 18:14:51 -0500

    Part 8: Added htmlspecialchars to echo statements to prevent scripting attacks. Part 6&7 completed with 5&8 respectively

--- display_discount.php
+++ display_discount.php
@@ -26,13 +26,13 @@ $discount_price_f = "$".number_format($discount_price, 2);
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo $product_description; ?></span><br>
+        <span><?php echo htmlspecialchars($product_description); ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo $list_price_f; ?></span><br>
+        <span><?php echo htmlspecialchars($list_price_f); ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo $discount_percentage_f; ?></span><br>
+        <span><?php echo htmlspecialchars($discount_percentage_f); ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo $discount_amount_f; ?></span><br>
